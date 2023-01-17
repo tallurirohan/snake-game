101ce5b5b5af51f707a94873728a49d0f2beb256
tallurirohan <113008429+tallurirohan@users.noreply.github.com>
2023-01-17 19:05:27 +0530

    Add files via upload

---
+++ gameframe.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class gameframe extends JFrame {
+    gameframe(){
+        gamepanel panel = new gamepanel();
+        this.add(panel);
+        this.setTitle("snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+
+    }
+}
